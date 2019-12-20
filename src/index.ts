bb34f219c69fc9f5410ff964fa405d149d6968b2
Wacim <wacim.yassine@syscobat.com>
2019-12-20 15:23:39 +0100

    trying to add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import {Column, Connection, createConnection, PrimaryColumn} from "typeorm";
 import {error} from "util";
+var cors = require('cors');
 
 let activeConnection: Connection;
 const ifcObjectController = require('./controller/ifcObjectController');
@@ -9,6 +10,7 @@ const ifcObjectController = require('./controller/ifcObjectController');
 createConnection().catch(error => console.log(error));
 const express = require("express");
 const app = express();
+app.use(cors);
 
 app.use(express.json());
 app.use('/ifcObject', ifcObjectController);
