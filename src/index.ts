596ff6d8de1777c8fa69666585ad566d40e7b9b4
Wacim <wacim.yassine@syscobat.com>
2019-12-20 16:16:18 +0100

    trying to add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import {Column, Connection, createConnection, PrimaryColumn} from "typeorm";
 import {error} from "util";
-var cors = require('cors');
+let cors = require('cors');
 
 let activeConnection: Connection;
 const ifcObjectController = require('./controller/ifcObjectController');
@@ -10,9 +10,16 @@ const ifcObjectController = require('./controller/ifcObjectController');
 createConnection().catch(error => console.log(error));
 const express = require("express");
 const app = express();
-app.use(cors);
 
 app.use(express.json());
+app.use(cors());
+/*app.use(function(req,res,next){
+    res.setHeader('Access-Control-Allow-Origin','*'); //http://localhost:3000,
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT,PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers','*');
+    res.setHeader('Access-Control-Allow-Credentials','true');
+    next();
+});*/
 app.use('/ifcObject', ifcObjectController);
 app.listen(3000, () => {
     console.log("Server running on port 3000");
